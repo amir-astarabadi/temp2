6c1085ae1ee94da5ca187a176b367fb41561b22b
New Name <new@email.com>
2025-04-09 09:48:32 +0330

    assert VerifyEmailNotification has been sent on register

--- tests/Feature/Controllers/Authentication/RegisterTest.php
+++ tests/Feature/Controllers/Authentication/RegisterTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Controllers\Authentication;
 
 use App\Models\User;
+use App\Notifications\VerifyEmailNotification;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Tests\TestCase;
 
@@ -10,6 +12,7 @@ class RegisterTest extends TestCase
 {
     public function test_happy_path(): void
     {
+        Notification::fake();
         $userData = [
             'accepted_contract' => true,
             'email' => 'example@gmail.com',
@@ -26,6 +29,8 @@ class RegisterTest extends TestCase
                 ->where('data.email', $userData['email'])
                 ->etc();
         });
+
+        Notification::assertSentTo(User::where('email', $userData['email'])->first(), VerifyEmailNotification::class);
     }
 
     public function test_duplicate_email_not_permitted(): void
@@ -38,7 +43,7 @@ class RegisterTest extends TestCase
             'password_confirmation' => 'password',
         ];
         $response = $this->postJson(route('user_register'), $userData);
-        
+
         $response->assertUnprocessable();
         $response->assertJson(function (AssertableJson $assertableJson) use ($userData) {
             $assertableJson
