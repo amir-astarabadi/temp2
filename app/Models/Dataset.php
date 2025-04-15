246bbe900b167a69ee1db99991ae6691b82caf59
New Name <new@email.com>
2025-04-15 23:46:11 +0330

    error handling for aws miss config

--- app/Models/Dataset.php
+++ app/Models/Dataset.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
@@ -12,6 +13,10 @@ class Dataset extends Model
 
     public function getFileUrlAttribute()
     {
-        return Storage::disk('dataset')->temporaryUrl($this->file_path, now()->addDays(1));
+        try{
+            return Storage::disk('dataset')->temporaryUrl($this->file_path, now()->addDays(1));
+        }catch(Exception $e){
+            return "no path set!";
+        }
     }
 }
