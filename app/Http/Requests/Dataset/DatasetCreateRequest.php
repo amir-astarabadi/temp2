fc1c39384fe55fe31a067f7eb2135b019e319a1c
New Name <new@email.com>
2025-04-25 19:56:26 +0330

    feat: Implement dataset deletion with MinIO dispatch and update DatasetResource to include project_id

--- app/Http/Requests/Dataset/DatasetCreateRequest.php
+++ app/Http/Requests/Dataset/DatasetCreateRequest.php
@@ -10,8 +10,11 @@ class DatasetCreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required','string','max:255', Rule::unique('datasets')->where(function ($query) {
-                return $query->where('user_id', $this->user()->getKey())->where('project_id', $this->input('project_id'));
+            'name' => ['required', 'string', 'max:255', Rule::unique('datasets')->where(function ($query) {
+                return $query
+                    ->where('user_id', $this->user()->getKey())
+                    ->where('project_id', $this->input('project_id'))
+                    ->whereNull('deleted_at');
             })],
             'description' => 'nullable|string',
             'dataset' => 'required|file|mimes:xlsx,xls,csv|max:102400',
