98b4035331572000b9ac438e71161793240b2aad
New Name <new@email.com>
2025-05-09 09:06:51 +0330

    make project description nullable

--- app/Http/Requests/Project/ProjectCreateRequest.php
+++ app/Http/Requests/Project/ProjectCreateRequest.php
@@ -11,7 +11,7 @@ class ProjectCreateRequest extends FormRequest
     {
         return [
             'name' => 'required|string|max:255',
-            'description' => 'string',
+            'description' => 'nullable|string',
             'user_id' => 'required'
         ];
     }
