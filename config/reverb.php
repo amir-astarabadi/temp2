9ccd946b00e988b3732135e55dc33f9b7a4d34c9
New Name <new@email.com>
2025-05-17 20:09:49 +0330

    feat: Update Reverb configuration for server settings and allowed origins

--- config/reverb.php
+++ config/reverb.php
@@ -29,8 +29,8 @@ return [
     'servers' => [
 
         'reverb' => [
-            'host' => env('REVERB_HOST', '0.0.0.0'),
-            'port' => env('REVERB_PORT', 8080),
+            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
+            'port' => env('REVERB_SERVER_PORT', 8080),
             'path' => env('REVERB_PATH', ''),
             'hostname' => env('REVERB_HOST'),
             'options' => [
@@ -87,7 +87,7 @@ return [
                     'scheme' => env('REVERB_SCHEME', 'https'),
                     'useTLS' => env('REVERB_SCHEME', 'https') === 'https',
                 ],
-                'allowed_origins' => ['*'],
+                'allowed_origins' => ['qanun.bio'],
                 'ping_interval' => env('REVERB_APP_PING_INTERVAL', 60),
                 'activity_timeout' => env('REVERB_APP_ACTIVITY_TIMEOUT', 30),
                 'max_message_size' => env('REVERB_APP_MAX_MESSAGE_SIZE', 10_000),
