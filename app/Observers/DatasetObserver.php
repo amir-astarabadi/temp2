70613f74a188e8b19f6fa6b2487114d2a64bc8cb
New Name <new@email.com>
2025-05-21 21:13:44 +0330

    refactor: Remove DatasetStatusChangedNotification and its associated view

--- app/Observers/DatasetObserver.php
+++ app/Observers/DatasetObserver.php
@@ -3,16 +3,13 @@
 namespace App\Observers;
 
 use App\Events\BroadcastDatasetStatusUpdateEvent;
-use Illuminate\Support\Facades\Log;
 use App\Models\Dataset;
-use App\Notifications\DatasetStatusChangedNotification;
 
 class DatasetObserver
 {
     public function saved(Dataset $dataset): void
     {
         if($dataset->isDirty('status')){
-            $dataset->user->notify(new DatasetStatusChangedNotification($dataset->getKey()));
             BroadcastDatasetStatusUpdateEvent::dispatch($dataset->id);        
         }
     }
